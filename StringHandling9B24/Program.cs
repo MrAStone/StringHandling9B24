de48c5ffc4b461e3f7b56138393faac369c68606
Al Stone <astone@heckgrammar.co.uk>
2024-10-11 09:58:04 +0100

    Add project files.

---
+++ StringHandling9B24/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace StringHandling9B24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = "This is a string";
+            string second = "This is a another string";
+            //Length of a string
+            Console.WriteLine(first.Length);
+            Console.WriteLine(second.Length);
+            //Position
+            Console.WriteLine(first[0]); // the first character in a string
+            Console.WriteLine(first[first.Length-1]);
+            Console.WriteLine(first.IndexOf('s')); //3 because it is the 4th character
+            Console.WriteLine(first.IndexOf('Z')); //-1 becauase it isn't there
+            Console.WriteLine(first[100]);
+
+
+
+        }
+    }
+}
