e25f13e739f00be91a353a89376b7bf2a1a5ef09
Al Stone <astone@heckgrammar.co.uk>
2024-10-14 11:21:43 +0100

    Complete

--- StringHandling9B24/Program.cs
+++ StringHandling9B24/Program.cs
@@ -14,9 +14,37 @@
             Console.WriteLine(first[first.Length-1]);
             Console.WriteLine(first.IndexOf('s')); //3 because it is the 4th character
             Console.WriteLine(first.IndexOf('Z')); //-1 becauase it isn't there
-            Console.WriteLine(first[100]);
+                                                   // Console.WriteLine(first[100]);
 
+            int userNumber;
+            Console.Write("Enter a number: ");
+            userNumber =Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine(userNumber);
+            double userDecimalNumber;
+            Console.Write("Enter a decimal number: ");
+            userDecimalNumber = Convert.ToDouble( Console.ReadLine());
+            Console.WriteLine(userDecimalNumber);
+            string numString = userNumber.ToString();
+            char myChar = 'A';
+            Console.WriteLine(Convert.ToInt32(myChar));
+            myChar = 'a';
+            Console.WriteLine(Convert.ToInt32(myChar));
+            myChar = '0';
+            Console.WriteLine(Convert.ToInt32(myChar));
 
+            //Find the ASCII Score for my name
+            string name;
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+            int sum = 0;
+            for(int i = 0;i<name.Length;i++)
+            {
+                sum +=Convert.ToInt32(name[i]);
+            }
+            Console.WriteLine(sum);
+
+            int numChar = 42;
+            Console.WriteLine(Convert.ToChar(numChar));
 
         }
     }
